68f8a743b89fda7adefc3f8ab65d7256df7182c4
Jonathan <jonathan.che@outlook.com>
2018-12-20 22:43:35 -0800

    Provider & BrowserRouter wraps App in index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -2,19 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 // rel path imports
 import App from './Components/App';
-import Card from './Components/Card';
 import store from './store';
 
 const router = (
-  <Provider>
-    <Router>
-      <Route path="/" exact component={App} />
-      <Route path="view/:cardId" component={Card} />
-    </Router>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
   </Provider>
 );
 
