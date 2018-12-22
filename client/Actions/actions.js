68cfdd08f7d70a9afdd5234e67cabd6ca0c533bb
Jonathan <jonathan.che@outlook.com>
2018-12-21 21:42:57 -0800

    action objects

--- client/Actions/actions.js
+++ client/Actions/actions.js
@@ -4,3 +4,25 @@ export const addCard = (card) => ({
   type: actionTypes.ADD_CARD,
   payload: card,
 });
+
+export const displayRandomCard = (card) => ({
+  type: actionTypes.DISPLAY_RANDOM_CARD,
+  payload: card,
+});
+
+export const displaySelectedCard = (card) => ({
+  type: actionTypes.DISPLAY_SELECTED_CARD,
+  payload: card,
+});
+
+export const allCards = () => ({
+  type: actionTypes.ALL_CARDS,
+});
+
+export const totalCards = () => ({
+  type: actionTypes.TOTAL_CARDS,
+});
+
+export const showRandomBoolean = () => ({
+  type: actionTypes.SHOW_RANDOM_BOOLEAN,
+});
