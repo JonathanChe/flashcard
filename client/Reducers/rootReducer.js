1c45d5d8700e914c16d033c4b0f02f5c679773d1
Jonathan <jonathan.che@outlook.com>
2018-12-21 21:35:58 -0800

    combining reducers

--- client/Reducers/rootReducer.js
+++ client/Reducers/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import mainReducer from './mainReducer';
+import cardReducer from './cardReducer';
 
 const rootReducer = combineReducers({
+  cardReducer,
   mainReducer,
   routing: routerReducer,
 });
