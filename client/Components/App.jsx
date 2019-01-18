91f0c0d8adea5553e8ada1efad77c0e62ce3f67f
Jonathan <jonathan.che@outlook.com>
2019-01-17 20:30:23 -0800

    successfully creates card

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios';
 import SearchAppBar from './NavBar';
 import Flashcards from './FlashCards';
 import AddCard from './AddCard';
 import FlashCard from './FlashCard';
 
-
-const App = (props) => {
-
+const App = () => {
   const handleSubmit = values => {
-    console.log('showing values', values);
+    axios.post('http://localhost:7777/addCard', values)
+      .then(res => console.log(res))
+      .catch(err => console.error('error posting ', err));
   };
 
   return (
@@ -17,10 +18,12 @@ const App = (props) => {
       <div>
         <SearchAppBar />
         <Route exact path="/" component={FlashCard} />
-        <Route path="/addCard" component={AddCard} />
+        <Route
+          path="/addCard"
+          render={(props) => <AddCard {...props} onSubmit={handleSubmit} />}
+        />
         <Route path="/random" component={FlashCard} />
         <Route path="/allCards" component={Flashcards} />
-
       </div>
 
     </Router>
--- server/server.js
+++ server/server.js
@@ -1,11 +1,34 @@
 require('dotenv').config();
 const express = require('express');
 const db = require('./postgresql/db');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const port = process.env.PORT || 7777;
 
 // intialize server
 const app = express();
+
+app.use(express.static('dist'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
+// cors
+app.use(cors());
+
+// add new cards
+app.post('/addCard', (req, res) => {
+  const { word, type, definition, sentence } = req.body;
+  const date = new Date();
+  db.one('INSERT INTO "Cards"("word", "type", "definition", "sentence", "timestamp") VALUES($1, $2, $3, $4, $5) RETURNING *', [word, type, definition, sentence, date])
+    .then(data => console.log('successfully created card', data))
+    .catch(err => console.error('error creating card ', err));
+});
 
 // start server
 app.listen(port, () => console.log(`listening on port ${port}`));
